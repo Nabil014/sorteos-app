97ee60452d36ad85d47f33e22393ff2683b18d9a
Nabil <nallis014@gmail.com>
2023-05-11 11:15:51 -0300

    change name app

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ export default function NavBar () {
     <nav className="flex justify-between items-center p-4 bg-[#FDFDFD]">
       <div className="flex items-center ">
         <img className="h-14" src="https://res.cloudinary.com/dc0bapngk/image/upload/v1683139633/trofeo_a2m1v9.jpg" alt="logo" />
-        <h3 className="ml-4 text-[#231F20] font-Bruno">sorteos.com</h3>
+        <h3 className="ml-4 text-[#231F20] font-Bruno">Sorteos app</h3>
       </div>
     </nav>
   )
